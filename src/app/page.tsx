f5236f9cb26c0af6162b5366994be91635f8ceca
Edward Ponce <eaponce.vanguards@gmail.com>
2025-03-31 23:17:06 -0700

    made logo larger, increased button size, added more service areas, fixed text on mobile showing up black

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export const metadata:Metadata = {
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div className="grid flex-row fixed w-[100%] grid-cols-3 grid-rows-1 min-h-20 max-h-30 text-black bg-black text-white z-1 ">
+      <div className="grid flex-row fixed w-[100%] grid-cols-3 grid-rows-1 min-h-20 max-h-30 bg-black text-white z-1 ">
         <div className="col-1 m-auto max-w-20">
           <h1><a href="#home">Home</a></h1>
         </div>
@@ -25,18 +25,14 @@ export default function Home() {
         </div>
       </div>
       <div className="flex w-full @container">
-        <div className="grid w-full min-h-200 max-h-300 grid-cols-7 grid-rows-7 content-center bg-[url(./img/wheel-rag.jpg)] bg-center bg-cover" id="home">
-        <div className="col-start-2 col-end-7 md:col-start-4 md:col-end-5  row-start-2 md:row-start-2 row-span-2 justify-center content-center ">
+        <div className="grid w-full min-h-200 max-h-300 grid-cols-7 grid-rows-4 content-center bg-[url(./img/wheel-rag.jpg)] bg-center bg-cover" id="home">
+        <div className="col-start-2 col-end-7 md:col-start-4 md:col-end-5  row-start-2 md:row-start-2 row-span-1 justify-center content-center ">
           <img className="size-30 margin-auto h-50 w-50 md:h-50 md:w-50 m-auto" src="boy.png"></img>
         </div>
           
         <div className="col-start-1 col-end-8 row-4 justify-center content-center m-auto --font-montserrat text-white">
-            <h1 className="text-center text-3xl md:text-5xl">Fresh Flow Mobile Auto Spa</h1>
           </div>
-          <div className="col-start-1 col-end-8 row-5 justify-center content-center">
-            <h1 className="text-center text-md md:text-2xl">Mobile Car Wash and Detailing</h1>
-          </div>
-          <div className=" col-start-2 md:col-start-3 col-end-7 md:col-end-6 row-6 justify-center content-center"><a href="https://app.squareup.com/appointments/book/huuxb13bg1wbt4/LEEDGNVTD70K2/start"><button className="block auto-w bg-linear-10 from-black to-purple-500 m-auto p-10 text-4xl cursor-pointer z-1" >Book Now!</button></a></div>
+          <div className=" col-start-2 md:col-start-3 col-end-7 md:col-end-6 row-3 justify-center content-center"><a href="https://app.squareup.com/appointments/book/huuxb13bg1wbt4/LEEDGNVTD70K2/start"><button className="block w-full bg-linear-10 from-black to-purple-500 m-auto p-10 text-4xl text-white cursor-pointer z-1" >Book Now!</button></a></div>
         </div>
       </div>
       
@@ -49,10 +45,10 @@ export default function Home() {
             <h1 className="text-3xl md:text-7xl content-center text-center">Standard</h1>
           </div>
           <div className="flex col-start-2 col-end-10 row-15 md:col-start-5  md:col-end-7  md:row-3 justify-center content-center bg-black ">
-            <h1 className="text-3xl md:text-7xl content-center text-center">Premium</h1>
+            <h1 className="text-3xl md:text-7xl content-center text-center text-white">Premium</h1>
           </div>
           <div className="grid col-start-2 col-end-10 row-33 md:col-start-7 md:col-end-9 md:row-3 justify-center content-center bg-black ">
-            <h1 className=" text-3xl md:text-7xl content-center text-center">Deluxe</h1>
+            <h1 className=" text-3xl md:text-7xl content-center text-center text-white">Deluxe</h1>
           </div>
 
           <div className="flex col-start-2 col-end-10 row-4 row-span-9 md:col-start-3  md:col-end-5 md:row-start-4 md:row-span-5 bg-linear-10 from-white to-gray-900">
@@ -105,7 +101,7 @@ export default function Home() {
             <p className="text-3xl md:text-6xl m-auto">$350-$1500</p>
           </div>
           <div className="flex col-start-2 col-end-10 row-51 md:row-10">
-            <p className="text-center m-auto md:text-4xl bg-black md:p-10">Final Prices Upon Vehicle Inspection</p>
+            <p className="text-center m-auto md:text-4xl text-white bg-black md:p-10">Final Prices Upon Vehicle Inspection</p>
           </div>
         </div>
       </div>
@@ -117,7 +113,7 @@ export default function Home() {
         </div>
         <div className="col-start-3 col-end-10 row-4 content-center justify-center z-1"><p className="text-md md:text-3xl text-center">☎️<a href="tel:+16263912351">(626) 391-2351</a></p></div>
         <div className="col-start-3 col-end-10 row-5 content-center justify-center z-1"><p className="text-md md:text-3xl text-center">📧 <a href="mailto:freshflowautospa@gmail.com">freshflowautospa@gmail.com</a></p></div>
-        <div className="col-start-3 col-end-10 row-6 content-center justify-center z-1"><p className="text-md md:text-3xl text-center">📍Service Areas: Mira Loma, Corona, Chino, Riverside, San Bernardino</p></div>
+        <div className="col-start-3 col-end-10 row-6 content-center justify-center z-1"><p className="text-md md:text-3xl text-center">📍Service Areas: Jurupa Valley, Corona, Eastvale, Chino, Ontario, Fontana, Pomonoa, Riverside, San Bernandino, and surrounding areas!</p></div>
         <div className="col-start-4 col-span-1  row-8 content-center justify-center z-1"><a href="https://www.yelp.com/biz/fresh-flow-mobile-auto-spa-mira-loma-3"><img src="Yelp.png" className="h-10 h-10 md:h-20 md:h-20 m-auto"></img></a></div>
         <div className="col-start-6 col-span-1  row-8 content-center justify-center z-1"><a href="https://www.instagram.com/freshflowmobileautospa/"><img src="insta.png" className="h-10 h-10 md:h-20 md:h-20 m-auto"></img></a></div>
         <div className="col-start-8 col-span-1  row-8 content-center justify-center z-1"><a href="https://www.instagram.com/freshflowmobileautospa/"><img src="facebook.png" className="h-10 h-10 md:h-20 md:h-20 m-auto"></img></a></div>
